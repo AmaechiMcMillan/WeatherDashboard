2e75e0440f190e5a908afd41b2bf9e0a5a79a606
Amaechi McMillan <amaechimcmillan@gmail.com>
2022-09-13 01:26:06 -0500

    added api calls

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let lat = 33.44;
 let lon = -94.04;
-let apiKey = '72d0adda79a4c922f5ddfc0788d12b1a'
+let apiKey = 'b783a74ee2f13afc2bb030d11bd281e5'
+let cityName = "Dallas"
+
+fetch('http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid={apiKey}')
+.then(function (response) {
+
+    return response.json()
+
+})
+.then(function (cityData){
+
+    console.log(cityData[0].lat);
+    console.log(cityData[0].lon);
+    
+
+})
 
 
 
